37bc9317f4f8dae9effb1a32d570733e26f83057
antowski <antowski@gmail.com>
2019-11-16 18:11:46 +0300

    Added tasks table on main page

--- src/main/java/strello/dao/JdbcTaskDao.java
+++ src/main/java/strello/dao/JdbcTaskDao.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import strello.model.Task;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,8 +23,20 @@ public class JdbcTaskDao implements TaskDao {
     @Override
     public List<Task> getTasks() {
 
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(
+                1,
+                LocalDate.of(2015 , 6, 1),
+                LocalDate.of(2015,6,2),
+                "Жора Спит",
+                "Создать Strello"));
+        tasks.add(new Task(
+                2,
+                LocalDate.of(2017 , 6, 1),
+                LocalDate.of(2018,6,2),
+                "Вася сторожит",
+                "Жору"));
 
-
-        return null;
+        return tasks;
     }
 }
--- src/main/java/strello/mvc/HomeController.java
+++ src/main/java/strello/mvc/HomeController.java
@@ -1,11 +1,14 @@
 package strello.mvc;
 
+import org.apache.velocity.tools.generic.DateTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import strello.service.StrelloService;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -19,7 +22,13 @@ public class HomeController {
 
     @RequestMapping(value = {"/","/home"}, method = RequestMethod.GET)
     public String home(ModelMap model) {
+
         model.addAttribute("tasks", strelloService.getAllTasks());
+
+        // DateTool for date formatting in velocity templates
+        // See *.vm for $date.format(...)
+        model.put("date", new DateTool());
+
         return "home";
     }
 
